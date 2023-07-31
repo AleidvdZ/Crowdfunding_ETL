b5e788081721b6bde309c1f180ccd933ab56a553
GarrettSteenwyk <g.m.steenwyk@gmail.com>
2023-07-30 22:24:56 -0500

    proofread schema

--- Starter_Files/crowdfunding_db_schema.sql
+++ Starter_Files/crowdfunding_db_schema.sql
@@ -5,26 +5,6 @@ CREATE TABLE contacts (
 	email varchar(60) NOT NULL
 );
 
-CREATE TABLE campaign (
-	cf_id INT Primary Key,
-	contact_id INT NOT NULL,
-	company_name varchar(90) NOT NULL,
-	description TEXT NOT NULL,
-	goal INT NOT NULL,
-	pledged INT NOT NULL,
-	outcome varchar(30) NOT NULL,
-	backers_count INT NOT NULL,
-	country varchar(5) NOT NULL,
-	currency varcahr(10) NOT NULL,
-	launched_date date NOT NULL,
-	end_date date NOT NULL,
-	catergory_id varchar(15) NOT NULL,
-	subcategory_id varchar(15) NOT NULL
-		Foreign Key (contact_id) references contacts(contact_id),
-		Foreign Key (category_id) references category(category_id),
-		Foreign Key (subcategory_id) references subcategory(subcategory_id)		
-);
-
 CREATE TABLE category (
 	category_id varchar Primary Key,
 	category varchar(30) NOT NULL
@@ -33,4 +13,24 @@ CREATE TABLE category (
 CREATE TABLE subcategory (
 	subcategory_id varchar Primary Key,
 	subcategory varchar(30) NOT NULL
+);
+
+CREATE TABLE campaign (
+	cf_id INT Primary Key,
+	contact_id INT NOT NULL,
+	company_name varchar(90) NOT NULL,
+	description TEXT NOT NULL,
+	goal float(53) NOT NULL,
+	pledged float(53) NOT NULL,
+	outcome varchar(30) NOT NULL,
+	backers_count INT NOT NULL,
+	country varchar(5) NOT NULL,
+	currency varchar(10) NOT NULL,
+	launched_date date NOT NULL,
+	end_date date NOT NULL,
+	category_id varchar(15) NOT NULL,
+	subcategory_id varchar(15) NOT NULL,
+		Foreign Key (contact_id) references contacts(contact_id),
+		Foreign Key (category_id) references category(category_id),
+		Foreign Key (subcategory_id) references subcategory(subcategory_id)		
 );
